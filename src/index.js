dc9eda23c4c158312b885a1bd8e7bc92df12c049
Roger Chappel <roger@rogerchappel.com>
2026-05-02 15:48:22 +1000

    feat: export public planner api

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { planOperation } from './planner.js';
+export { recommendScopes, explainScopes, scopeMatrix } from './scopes.js';
+export { redactObject, redactText } from './redact.js';
+export { scanText, assertNoSecrets } from './secrets.js';
+export { adapterStatus, WorkspaceAdapter } from './adapters/interface.js';
